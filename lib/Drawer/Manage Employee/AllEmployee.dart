2b2046ac8b4afbf4e076aff202c46d1db3fa8623
flutterap <bishaltureha123@gmail.com>
2023-02-15 15:50:50 +0530

    new ui

--- lib/Drawer/Manage Employee/AllEmployee.dart
+++ lib/Drawer/Manage Employee/AllEmployee.dart
@@ -30,234 +30,33 @@ class AllEmployee extends StatelessWidget {
         ),
         body: Padding(
           padding: EdgeInsets.only(top: 90.h, left: 10.w, right: 10.w),
-          child: Column(
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Image.asset('Assets/total_employees.png'),
-                      SizedBox(
-                        width: 15.w,
-                      ),
-                      Text(
-                        'Employee 1',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w700, fontSize: 17.sp),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 25,
-                    width: 100.w,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xffD4C00B),
-                      ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, 'Addtask1');
-                      },
-                      child: Text(
-                        'Add Task',
-                        style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w700),
-                      ),
+          child: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                leading: Image.asset('Assets/total_employees.png'),
+                trailing: SizedBox(
+                  height: 25,
+                  width: 100.w,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xffD4C00B),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'Addtask1');
+                    },
+                    child: Text(
+                      'Add Task',
+                      style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 12.sp,
+                          fontWeight: FontWeight.w700),
                     ),
                   ),
-                ],
-              ),
-              SizedBox(
-                height: 5.h,
-              ),
-              Divider(
-                height: 30.h,
-                thickness: 1,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Image.asset('Assets/total_employees.png'),
-                      SizedBox(
-                        width: 15.w,
-                      ),
-                      Text(
-                        'Employee 2',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w700, fontSize: 17.sp),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 25.h,
-                    width: 100.w,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xffD4C00B),
-                      ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, 'Addtask1');
-                      },
-                      child: Text(
-                        'Add Task',
-                        style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w700),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 5.h,
-              ),
-              Divider(
-                height: 30.h,
-                thickness: 1,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Image.asset('Assets/total_employees.png'),
-                      SizedBox(
-                        width: 15.w,
-                      ),
-                      Text(
-                        'Employee 3',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w700, fontSize: 17.sp),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 25.h,
-                    width: 100.w,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xffD4C00B),
-                      ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, 'Addtask1');
-                      },
-                      child: Text(
-                        'Add Task',
-                        style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w700),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 5.h,
-              ),
-              Divider(
-                height: 30.h,
-                thickness: 1,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Image.asset('Assets/total_employees.png'),
-                      SizedBox(
-                        width: 15.w,
-                      ),
-                      Text(
-                        'Employee 4',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w700, fontSize: 17.sp),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 25.h,
-                    width: 100.w,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xffD4C00B),
-                      ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, 'Addtask1');
-                      },
-                      child: Text(
-                        'Add Task',
-                        style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w700),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 5.h,
-              ),
-              Divider(
-                height: 30.h,
-                thickness: 1,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Image.asset('Assets/total_employees.png'),
-                      SizedBox(
-                        width: 15.w,
-                      ),
-                      Text(
-                        'Employee 5',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w700, fontSize: 17.sp),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 25.h,
-                    width: 100.w,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xffD4C00B),
-                      ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, 'Addtask1');
-                      },
-                      child: Text(
-                        'Add Task',
-                        style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w700),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 5.h,
-              ),
-              Divider(
-                height: 30.h,
-                thickness: 1,
-              ),
-            ],
+                ),
+                title: Text('Employee ${index + 1}'),
+              );
+            },
           ),
         ));
   }
